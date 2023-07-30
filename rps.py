ec62244dca75a48a4c4ee8117926dce5cca08a31
Peter Kimberley <p3t3rk1m0@gmail.com>
2023-07-30 21:35:41 +0100

    - I have added the start of the rock, paper, scissors   game, and have set out the initial game rules.
    Signed-off-by: Peter Kimberley <p3t3rk1m0@gmail.com>

---
+++ rps.py
@@ -0,0 +1,17 @@
+# Rock paper scissors game
+
+
+# Imports extra python functions in standard libraries
+import random
+import time
+
+rock = 1 
+paper = 2 
+scissors = 3
+
+# These ar the initial rules of the game
+names = {rock: "Rock", paper: "paper", scissors: "Scissors"}
+rules = {rock: scissors, paper: rock, scissors: paper}
+
+player_score = 0
+computer_score = 0
