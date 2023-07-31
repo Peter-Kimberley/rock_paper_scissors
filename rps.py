21f81d98dbb424ed49b7f2a916034b98bdfe4d1f
Peter Kimberley <p3t3rk1m0@gmail.com>
2023-07-31 21:36:39 +0100

    -I have added the 3 functions start,  game, and move. I have also a try  statement.
    Signed-off-by: Peter Kimberley <p3t3rk1m0@gmail.com>

--- rps.py
+++ rps.py
@@ -15,3 +15,30 @@ rules = {rock: scissors, paper: rock, scissors: paper}
 
 player_score = 0
 computer_score = 0
+
+# Defining the start function
+def start():
+    print("Let's play a game ofrock, paper, scissors")
+    while game():
+        pass
+    scores()
+
+# Defining the the game function
+def game():
+    player = move()
+    computer = random.randint(1, 3)
+    result (playe, computer)
+    return play_again()
+
+# Defining the move function
+def move():
+    while True:
+        print()
+        player = raw_input("Rock = 1\nPaper = 2\nScissors =3\nMake a move: " )
+        try:
+            player = int(player)
+            if player in (1,2,3):
+                return player
+        except ValueError:
+            pass
+        print("Oops! I didn't understand that. Please enter 1, 2, or 3.")
